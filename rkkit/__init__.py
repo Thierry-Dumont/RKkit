a16e46f79475a15917d8f7da99e62aa569fcb6f0
Thierry Dumont <tdumont@math.univ-lyon1.fr>
2020-09-25 14:17:30 +0200

    remove __init__

--- rkkit/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-from sage.all import *
-from .RKTrees    import *
-from .RKformula  import *
-from .RKcolloc  import colloc
-from .RKplot  import *
-from .RKExceptions import *
-from .RKPolutilities import *
-
