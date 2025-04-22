b8bd83d7b71a04ea5887be3010672fa9ce7f80c2
Thierry Dumont <thierry@thierry-dumont.fr>
2025-04-22 12:03:35 +0200

    ok, mais des trucs à améliorer

--- rkkit/RKcolloc.py
+++ rkkit/RKcolloc.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from sage.all import *
 from .RKRungeKutta import RungeKutta
 from .RKExceptions import *
---
+++ rkkit/__init__.py
@@ -0,0 +1,5 @@
+from sage.all import *
+from .RKformula import RKformula
+from .RKcolloc import colloc
+from .RKplot import RKplot
+__all__=["RKRungeKutta","RKformula","RKplot","RKcolloc"]
