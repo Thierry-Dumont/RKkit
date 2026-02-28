597a896b1e07e9a986e416a57c15a42b388ab471
Thierry Dumont <thierry@thierry-dumont.fr>
2026-02-28 10:14:18 +0100

    not usefull

--- RKkit/rkkit/__init__.py
+++ RKkit/rkkit/__init__.py
@@ -3,5 +3,8 @@ from .RKformula import RKformula
 from .RKcolloc import colloc
 from .RKplot import RKplot
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.rings.qqbar import *
+import sage.rings.polynomial.laurent_polynomial_ring
+from sage.functions.orthogonal_polys import legendre_P
 
 __all__=["RKRungeKutta","RKformula","RKplot","RKcolloc"]
