17bb8026eb0561c5f7c4021750b7dbf9195cc665
Thierry Dumont <thierry@thierry-dumont.fr>
2026-03-02 23:00:01 +0100

    fix imports

--- RKkit/rkkit/__init__.py
+++ RKkit/rkkit/__init__.py
@@ -2,6 +2,7 @@
 from .RKformula import RKformula
 from .RKcolloc import colloc
 from .RKplot import RKplot
+from .RKRungeKutta import RungeKutta
 #
 from sage.rings.qqbar import *
 from sage.rings.polynomial import laurent_polynomial_ring
