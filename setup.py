02628c6697131ddec398408e09e8ed5f90062268
Thierry Dumont <thierry@thierry-dumont.fr>
2026-08-28 07:26:50 +0200

    setup et pyproject

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 setup(
     name='RKkit',
-    version='0.1',    
-    description='Playeing with Runge-Kutta methods',
+    version='0.1.0',
+    description='Playing with Runge-Kutta methods',
     url='https://github.com/Thierry-Dumont/RKkit',
     author='Thierry Dumont',
-    author_email='tdumont@math.cnts.fr',
+    author_email='tdumont@math.cnrs.fr',
     license='GNU GENERAL PUBLIC LICENSE Version 3',
     packages=['RKkit'],
     install_requires=[],
-    extras_require={"passagemath":["passagemath-standard"],},
+    extras_require={"passagemath":["passagemath-standard","passagemath-plot",
+                                   "passagemath-symbolics",
+                                   "passagemath-categories",
+                                   "passagemath-combinat",
+                                   "passagemath-flint",
+                                   "passagemath-environment",
+                                   "passagemath-singular",],},
 )
