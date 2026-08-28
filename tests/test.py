15d05de915c09d00bbab05915de0e3a07f67e277
Thierry Dumont <thierry@thierry-dumont.fr>
2026-08-28 18:31:51 +0200

    modifs to pyproject

--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,26 @@
 #
-import pytest
+import pytest, sage.all
 #
-from RKkit.methods.formulas import *
-from RKkit.rkkit.RKcolloc import *
-from RKkit.rkkit import *
+from RKkit import *
+
 #
 class TestRKkit:
     """ Test computation of all known properties for a set of known
     classical methods """
     def testSetOfRKformulas(self):
         """ All known properties mus be computed with all methods.formulas."""
-        cformulas=[Radau5(),Gauss4(),Radau2a(),RK4(),
-                   SDIRK3(),SDIRK5(),Lobatto4()]
+        cformulas=[formulas.Radau5(),formulas.Gauss4(),formulas.Radau2a(),
+                   formulas.RK4(),
+                   formulas.SDIRK3(),formulas.SDIRK5(),formulas.Lobatto4()]
         Bads=[]
         #
         for f in cformulas:
+            print(r)
             try:
+                
                 F=RKformula(f)
                 F.compute_all_properties()
             except:
                 Bads.append(f.Title)
         assert len(Bads) == 0
+
