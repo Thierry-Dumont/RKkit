3fa6358353a60b27559c882418d903315e64f0e5
Thierry Dumont <thierry@thierry-dumont.fr>
2026-08-31 13:22:33 +0200

    typo

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ class TestRKkit:
     """ Test computation of all known properties for a set of known
     classical methods """
     def testSetOfRKformulas(self):
-        """ All known properties mus be computed with all methods.formulas."""
+        """ All known properties must be computed with all methods."""
         cformulas=[formulas.Radau5(),formulas.Gauss4(),formulas.Radau2a(),
                    formulas.RK4(),
                    formulas.SDIRK3(),formulas.SDIRK5(),formulas.Lobatto4()]
